c3b2679968fc20d16e4323d664c2ba95c33440f8
knemo0914-Dax <knemo0914@gmail.com>
2025-11-30 19:16:27 +0100

    Add files via upload

---
+++ Arrays.cxx
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int scores[4] ={11,12,13,14};
+    cout << scores[0] <<endl;
+    cout << scores[1] <<endl;
+    cout << scores[2] <<endl;
+    cout << scores[3] <<endl;
+    return 0;
+    }
